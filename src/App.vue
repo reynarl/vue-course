4071463bf22b0adc51babe845b847fe6810b8c50
Reeyna <reeynarl@gmail.com>
2023-12-04 14:43:29 -0600

    Add pagination buttons

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,45 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 import BlogPost from './components/BlogPost.vue';
+import ButtonPagination from './components/ButtonPagination.vue';
 
 
 const posts = ref([])
+const favorite = ref('')
+const start = ref(0)
+const end = ref(10)
 
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then((res)=> res.json())
 .then((data)=> posts.value = data)
 
-const favorite = ref('')
 
 const changeFavoritePost = (title) => {
   favorite.value = title
 }
+
+//buttons
+const handleChangeBtnNext = () => {
+  // if(end.value >= posts.value.length){
+  //   end.value = 10
+  //   start.value = 0
+  // } else {
+  //   end.value = end.value + 10
+  //   start.value = start.value + 10
+  // }
+
+  end.value = end.value + 10
+  start.value = start.value + 10
+}
+
+const handleChangeBtnPrev = () => {
+  end.value = end.value - 10
+  start.value = start.value - 10
+}
+
+//usamos computed para estar a la escucha de si existen cambios de la longitud de los posts
+const maxLength = computed(() => posts.value.length)
 
 </script>
 
@@ -22,14 +47,22 @@ const changeFavoritePost = (title) => {
   <div class="container">
     <h1>APP</h1>
     <p>Favorito: {{ favorite ? favorite : 'Clickea un post favorito' }}</p>
-  <BlogPost 
-  v-for="post in posts"
-    :id=post.id
-    :title=post.title
-    :body=post.body
-    :colorText=post.colorText
-    @changeFavoritePost = changeFavoritePost
-  >
-</BlogPost>
+    <ButtonPagination 
+      class="mb-3"
+      :start="start"
+      :end="end"
+      :maxLength="maxLength"
+      @handleChangeBtnNext = "handleChangeBtnNext"
+      @handleChangeBtnPrev = "handleChangeBtnPrev"
+    />
+    <BlogPost 
+    v-for="post in posts.slice(start,end)"
+      :id="post.id"
+      :title="post.title"
+      :body="post.body"
+      :colorText="post.colorText"
+      @changeFavoritePost = "changeFavoritePost"
+    >
+  </BlogPost>
   </div>
 </template>
---
+++ src/components/ButtonPagination.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+const emit = defineEmits(['handleChangeBtnNext', 'handleChangeBtnPrev'])
+
+//activar emit desde otra funcion
+const activar = () => {
+  emit('handleChangeBtnNext')
+}
+
+defineProps(['start', 'maxLength', 'end'])
+
+</script>
+
+<template>
+  <div class="btn-group" role="group" aria-label="Basic example">
+    <button 
+        :disabled="start === 0" 
+        type="button" 
+        class="btn btn-outline-primary" 
+        @click="emit('handleChangeBtnPrev')"
+    >Left</button>
+
+    <button 
+        :disabled="end === maxLength" 
+        type="button" 
+        class="btn btn-outline-primary" 
+        @click="activar"
+    >Right</button>
+  </div>
+</template>
